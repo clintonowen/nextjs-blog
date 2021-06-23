ca1d74a7b7fe6f27fd032881dcf54a4c2f195332
Clinton Owen <cowen@groupon.com>
2021-06-23 17:11:09 -0500

    feat: Pre-rendering and Data Fetching

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: config => {
+    return {
+      ...config,
+      module: {
+        ...(config.module || {}),
+        noParse: [
+          ...(config.module.noParse || []),
+          require.resolve('prettier/parser-typescript.js'),
+        ],
+      },
+    };
+  },
 };
